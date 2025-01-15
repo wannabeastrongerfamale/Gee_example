b2f97f51e8e4b7f3b4908af0709527414b02b1a3
wannabeastrongerfamale <2812855561@qq.com>
2025-01-15 17:05:55 +0800

    day4-路由分组控制：以相同前缀来区分路由分组，并支持分组嵌套

--- gee/gee.go
+++ gee/gee.go
@@ -7,21 +7,50 @@ import(
 type HandlerFunc func(c *Context)
 
 type Engine struct{
-	router 	*Router//路由
+	*RouterGroup	//嵌入RouterGroup---可以直接使用RouterGroup的成员方法
+	router 	*Router	//路由
+	groups []*RouterGroup	//分组列表
+}
+
+type RouterGroup struct{
+	prefix string
+	parent *RouterGroup
+	engine *Engine
 }
 
 //New构造gee.Engine
 func New() *Engine{
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	//创建一个默认路由组--prefix为空
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+//Group嵌套
+func (group *RouterGroup) Group(prefix string) *RouterGroup{
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc){
+	pattern := group.prefix + comp
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 //添加GET、POST请求的路由
-func (engine *Engine) GET(pattern string, handler HandlerFunc){
-	engine.router.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc){
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc){
-	engine.router.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc){
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
--- main.go
+++ main.go
@@ -10,27 +10,36 @@ func main(){
 	//创建gee实例
 	r := gee.New()
 	//使用GET、POST添加路由
-	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>\n")
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>\n")
 	})
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 		})
-	})
-	//动态路由
-	r.GET("/hello/:name", func(c *gee.Context) {
-		// expect /hello/geektutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
+	
 	//使用Run启动Web服务
 	r.Run(":9999")
 }
